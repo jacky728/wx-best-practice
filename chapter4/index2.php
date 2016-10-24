11b4b0124e10aae31a8619f736b2e675ff27649c
Jacky <jacky.yang@centflag.com>
2016-10-24 17:38:43 +0800

    Add function https_request

--- chapter4/index2.php
+++ chapter4/index2.php
@@ -390,5 +390,19 @@ $item_str
         return $result;
     }
 
+
+    function https_request($url)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        if (curl_errno($curl)) {return 'ERROR '.curl_error($curl);}
+        curl_close($curl);
+        return $data;
+    }
+
 }
 ?>
